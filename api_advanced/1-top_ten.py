74bce83daf36b29e9fa712f4ebc902b08a173bdd
Justineneema <j.neema@alustudent.com>
2025-02-21 23:29:08 +0200

    api_advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,33 +1,20 @@
 #!/usr/bin/python3
 """
-Fetching the hot top 10 posts in a given subreddit
+Fetching the hot top 10 post in a given subreddit
 """
 import requests
 
 
 def top_ten(subreddit):
-    """Prints 'OK' if the subreddit exists, otherwise prints 'None'."""
-    if not subreddit or not isinstance(subreddit, str):
-        print("OK")
-        return
-
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Fab'}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code != 200:
-            print("None")
-            return
 
-        data = response.json().get("data", {}).get("children", [])
-
-        # If there are posts, print "OK"
-        if data:
-            print("OK")
-        else:
-            print("None")
-
-    except (requests.RequestException, ValueError, KeyError):
-        print("None")
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        value = response.json()
+        datas = value['data']['children']
+        for data in datas:
+            title = data['data']['title']
+            print(title)
+    else:
+        print('None')
