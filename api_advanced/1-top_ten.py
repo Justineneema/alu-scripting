1051e4a59ccd2c338d6cb8b8b9da45e81eb748f7
Justineneema <j.neema@alustudent.com>
2025-02-21 23:14:51 +0200

    api_advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -11,10 +11,21 @@ def top_ten(subreddit):
 
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
+    #     value = response.json()
+    #     datas = value['data']['children']
+    #     for data in datas:
+    #         title = data['data']['title']
+    #         print(title)
+    # else:
+    #     print('None')
+     try:
         value = response.json()
-        datas = value['data']['children']
-        for data in datas:
-            title = data['data']['title']
-            print(title)
-    else:
-        print('None')
+        datas = value.get('data', {}).get('children', [])
+
+        if not datas:
+            print("None")
+        else:
+            print("OK")  # Print "OK" instead of post titles
+
+    except (ValueError, KeyError):
+        print("None")
