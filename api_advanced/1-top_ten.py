3c3c9f7503a99b14c7356f9b3bac3b2bc91d1af3
Justineneema <j.neema@alustudent.com>
2025-02-21 23:05:33 +0200

    api_advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
 """
-Fetching the hot top 10 post in a given subreddit
+A script to fetch the top 10 hot posts from a given subreddit using Reddit's API.
 """
 import requests
 
 
 def top_ten(subreddit):
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Fab'}
+    """Fetches and prints the titles of the top 10 hot posts from a given subreddit."""
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    headers = {'User-Agent': 'Custom-User-Agent/1.0'}
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # Check if request is successful
     if response.status_code == 200:
-        value = response.json()
-        datas = value['data']['children']
-        for data in datas:
-            title = data['data']['title']
-            print(title)
+        try:
+            value = response.json()
+            datas = value.get('data', {}).get('children', [])
+
+            if not datas:
+                print("None")
+                return
+
+            for data in datas:
+                title = data['data'].get('title', "None")
+                print(title)
+
+        except ValueError:
+            print("None")  
     else:
-        print('None')
+        print("None")  
